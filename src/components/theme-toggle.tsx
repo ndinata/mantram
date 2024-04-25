0632f080a28db2d304cc9f78a091fe8885b2425b
Nico Dinata <nicodnt@icloud.com>
2024-04-25 17:22:45 +1000

    make theme toggle controlled to fix issue on ios/safari

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
+  const [open, setOpen] = React.useState(false);
   const [theme, setThemeState] = React.useState<
     "theme-light" | "dark" | "system"
   >("theme-light");
@@ -28,9 +29,14 @@ export function ThemeToggle() {
   }, [theme]);
 
   return (
-    <DropdownMenu>
+    <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="h-8 w-8">
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-8 w-8"
+          onClick={() => setOpen((o) => !o)}
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
