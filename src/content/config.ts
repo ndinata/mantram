b69be15f940ead214c249c79324b2b907700b736
Nico Dinata <nicodnt@icloud.com>
2024-05-02 21:33:17 +1000

    set up posts content collection

---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { z, defineCollection } from "astro:content";
+
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    /** Title of the mantram. */
+    title: z.string(),
+    /** Subtitle of the mantram title. */
+    subtitle: z.string(),
+    /** Pinyin of the mantram title. */
+    titlePinyin: z.string().optional(),
+    /** Date the mantram was published. */
+    publishDate: z.date(),
+    /** Last time the mantram was updated. */
+    lastUpdatedDate: z.date(),
+    /** Additional context to the mantram. */
+    context: z
+      .object({
+        title: z.string(),
+        body: z.string(),
+      })
+      .optional(),
+  }),
+});
+
+export const collections = {
+  posts: postsCollection,
+};
