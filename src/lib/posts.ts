23f21f871e01dda1fabe7dcf55f1c1ca7e5866e2
Nico Dinata <nicodnt@icloud.com>
2024-05-26 11:40:39 +1000

    improve method to determine post numbering

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -4,9 +4,26 @@ import { getCollection } from "astro:content";
 export async function getPostsCollectionByPriority(
   ord: "asc" | "desc" = "desc",
 ) {
-  return (await getCollection("posts")).toSorted((a, b) => {
-    return ord === "asc"
-      ? a.data.priority - b.data.priority
-      : b.data.priority - a.data.priority;
-  });
+  return (await getCollection("posts"))
+    .toSorted((a, b) => {
+      return ord === "asc"
+        ? a.data.priority - b.data.priority
+        : b.data.priority - a.data.priority;
+    })
+    .map((item, index) => ({ ...item, position: adjustPosition(index) }));
+}
+
+/**
+ * Sets the rule for post numbering/positions.
+ *
+ * This allows for custom numbering rules instead of the default (index + 1),
+ * e.g. '3b' instead of a '4' for post positions.
+ */
+function adjustPosition(index: number): string {
+  switch (index + 1) {
+    // case 4:
+    //   return "3b";
+    default:
+      return (index + 1).toString();
+  }
 }
