1581e591bbbc295407636635e1b7b825002cf652
Nico Dinata <nicodnt@icloud.com>
2024-06-03 18:19:19 +1000

    install wasm parser in astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import wasm from "vite-plugin-wasm";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({ applyBaseStyles: false })],
+  vite: {
+    plugins: [wasm()],
+  },
   site: "https://mantram.blog",
   prefetch: true,
 });
