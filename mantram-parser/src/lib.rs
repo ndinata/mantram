d92c41a939d817f3221bd4cadd16838762b466a9
Nico Dinata <nicodnt@icloud.com>
2024-06-03 14:15:29 +1000

    complete impl of parser funcs

--- mantram-parser/src/lib.rs
+++ mantram-parser/src/lib.rs
@@ -3,13 +3,21 @@ use std::string::ToString;
 
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take, take_until, take_while};
-use nom::character::complete::{alpha1, line_ending, multispace0};
-use nom::combinator::{map_res, recognize, value};
-use nom::multi::{many_till, separated_list0};
+use nom::character::complete::{alpha1, anychar, line_ending};
+use nom::combinator::{map, map_res, recognize, value};
+use nom::multi::{many0, many_till, separated_list0};
 use nom::sequence::{delimited, terminated};
 use nom::IResult;
 use strum_macros::EnumString;
 // use wasm_bindgen::prelude::*;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Character {
+    Hanzi { char: char, sub: String },
+    Punc(Punctuation),
+    Context(String),
+    Linebreak,
+}
 
 pub fn parse_mantram_string(input: &str) -> Result<Vec<Character>, String> {
     let (_, chars) = separated_list0(tag("\n"), block_characters)(input)
@@ -25,22 +33,29 @@ fn block_characters(input: &str) -> IResult<&str, Vec<Character>> {
 }
 
 fn subtitle_line(input: &str) -> IResult<&str, Vec<&str>> {
-    fn non_alpha(i: &str) -> IResult<&str, &str> {
-        take_while(|c: char| !c.is_alphabetic() && c != '\n')(i)
-    }
-
-    terminated(separated_list0(non_alpha, alpha1), multispace0)(input)
+    many0(terminated(alpha1, take_while(|c: char| !c.is_alphabetic())))(input)
 }
 
 fn hanzi_line_chars<'a>(
     input: &'a str,
     subs: Vec<&'a str>,
 ) -> IResult<&'a str, Vec<Character>> {
-    let (input, chars) = many_till(take(1usize), line_ending)(input)?;
+    let mut subs = subs.iter();
 
-    // TODO
+    let (input, (chars, _)) = many_till(
+        alt((
+            punc,
+            context,
+            linebreak,
+            map(anychar, |c: char| Character::Hanzi {
+                char: c,
+                sub: subs.next().unwrap().to_string(),
+            }),
+        )),
+        line_ending,
+    )(input)?;
 
-    Ok((input, vec![]))
+    Ok((input, chars))
 }
 
 fn punc(input: &str) -> IResult<&str, Character> {
@@ -61,17 +76,6 @@ fn linebreak(input: &str) -> IResult<&str, Character> {
         Character::Linebreak,
         alt((tag("<br/>"), tag("<br />"), tag("<br>"))),
     )(input)
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Character {
-    Hanzi {
-        char: &'static str,
-        sub: &'static str,
-    },
-    Punc(Punctuation),
-    Context(String),
-    Linebreak,
 }
 
 #[derive(Debug, Clone, PartialEq, EnumString, strum_macros::Display)]
@@ -108,58 +112,58 @@ kuan ce cai phu sat.
 
         let expected = vec![
             Character::Hanzi {
-                char: "般",
-                sub: "po",
+                char: '般',
+                sub: "po".to_string(),
             },
             Character::Hanzi {
-                char: "若",
-                sub: "jo",
+                char: '若',
+                sub: "jo".to_string(),
             },
             Character::Hanzi {
-                char: "波",
-                sub: "po",
+                char: '波',
+                sub: "po".to_string(),
             },
             Character::Hanzi {
-                char: "羅",
-                sub: "luo",
+                char: '羅',
+                sub: "luo".to_string(),
             },
             Character::Hanzi {
-                char: "蜜",
-                sub: "mi",
+                char: '蜜',
+                sub: "mi".to_string(),
             },
             Character::Hanzi {
-                char: "多",
-                sub: "tuo",
+                char: '多',
+                sub: "tuo".to_string(),
             },
             Character::Hanzi {
-                char: "心",
-                sub: "sin",
+                char: '心',
+                sub: "sin".to_string(),
             },
             Character::Hanzi {
-                char: "經",
-                sub: "cing",
+                char: '經',
+                sub: "cing".to_string(),
             },
             Character::Punc(Punctuation::Period),
             Character::Linebreak,
             Character::Hanzi {
-                char: "觀",
-                sub: "kuan",
+                char: '觀',
+                sub: "kuan".to_string(),
             },
             Character::Hanzi {
-                char: "自",
-                sub: "ce",
+                char: '自',
+                sub: "ce".to_string(),
             },
             Character::Hanzi {
-                char: "在",
-                sub: "cai",
+                char: '在',
+                sub: "cai".to_string(),
             },
             Character::Hanzi {
-                char: "菩",
-                sub: "phu",
+                char: '菩',
+                sub: "phu".to_string(),
             },
             Character::Hanzi {
-                char: "薩",
-                sub: "sat",
+                char: '薩',
+                sub: "sat".to_string(),
             },
             Character::Punc(Punctuation::Period),
         ];
@@ -172,8 +176,9 @@ kuan ce cai phu sat.
     #[test]
     fn parsing_subtitle_line_works() {
         let input = "ciu hu shan sin ..., yi shen li khu nan.\n";
-        let (_, subs) = subtitle_line(input).unwrap();
+        let (input, subs) = subtitle_line(input).unwrap();
 
+        assert_eq!("", input);
         assert_eq!(
             vec!["ciu", "hu", "shan", "sin", "yi", "shen", "li", "khu", "nan"],
             subs
@@ -186,47 +191,48 @@ kuan ce cai phu sat.
 救護善信（念自己的名字），一身離苦難。
 ";
         let (input, subs) = subtitle_line(input).unwrap();
-        let (_, chars) = hanzi_line_chars(input, subs).unwrap();
+        let (input, chars) = hanzi_line_chars(input, subs).unwrap();
 
+        assert_eq!("", input);
         assert_eq!(
             vec![
                 Character::Hanzi {
-                    char: "救",
-                    sub: "ciu"
+                    char: '救',
+                    sub: "ciu".to_string()
                 },
                 Character::Hanzi {
-                    char: "護",
-                    sub: "hu"
+                    char: '護',
+                    sub: "hu".to_string()
                 },
                 Character::Hanzi {
-                    char: "善",
-                    sub: "shan"
+                    char: '善',
+                    sub: "shan".to_string()
                 },
                 Character::Hanzi {
-                    char: "信",
-                    sub: "sin"
+                    char: '信',
+                    sub: "sin".to_string()
                 },
                 Character::Context("（念自己的名字）".to_string()),
                 Character::Punc(Punctuation::Comma),
                 Character::Hanzi {
-                    char: "一",
-                    sub: "yi"
+                    char: '一',
+                    sub: "yi".to_string()
                 },
                 Character::Hanzi {
-                    char: "身",
-                    sub: "shen"
+                    char: '身',
+                    sub: "shen".to_string()
                 },
                 Character::Hanzi {
-                    char: "離",
-                    sub: "li"
+                    char: '離',
+                    sub: "li".to_string()
                 },
                 Character::Hanzi {
-                    char: "苦",
-                    sub: "khu"
+                    char: '苦',
+                    sub: "khu".to_string()
                 },
                 Character::Hanzi {
-                    char: "難",
-                    sub: "nan"
+                    char: '難',
+                    sub: "nan".to_string()
                 },
                 Character::Punc(Punctuation::Period)
             ],
@@ -252,16 +258,20 @@ kuan ce cai phu sat.
     #[should_panic]
     #[case("?", Character::Punc(Punctuation::Period))]
     fn parsing_punc_works(#[case] input: &str, #[case] expected: Character) {
-        let output = punc(input).unwrap();
-        assert_eq!(expected, output.1);
+        let (i, char) = punc(input).unwrap();
+
+        assert_eq!("", i);
+        assert_eq!(expected, char);
     }
 
     #[rstest]
     #[case("（念自己的名字）")]
     #[case("（）")]
     fn parsing_context_works(#[case] input: &str) {
-        let output = context(input).unwrap();
-        assert_eq!(Character::Context(input.to_string()), output.1);
+        let (i, char) = context(input).unwrap();
+
+        assert_eq!("", i);
+        assert_eq!(Character::Context(input.to_string()), char);
     }
 
     #[rstest]
@@ -269,7 +279,9 @@ kuan ce cai phu sat.
     #[case("<br />")]
     #[case("<br>")]
     fn parsing_linebreak_works(#[case] input: &str) {
-        let output = linebreak(input).unwrap();
-        assert_eq!(Character::Linebreak, output.1);
+        let (i, char) = linebreak(input).unwrap();
+
+        assert_eq!("", i);
+        assert_eq!(Character::Linebreak, char);
     }
 }
